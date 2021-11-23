926450b3af57af3bd237c079d27a9952281fe067
Noah <noah.van.ommen@hotmail.com>
2021-11-23 12:47:07 +0100

    BUTTON UPDATE
    Gamer inputs ADDED

--- src/main/include/ControllerIdentificationDictionairy.hpp
+++ src/main/include/ControllerIdentificationDictionairy.hpp
@@ -8,13 +8,14 @@
 
 // created includes
 #include "exr_definitions.h"
+#include "button_definitions.h"
 
 class ControllerIdentificationDictionairy  
 {
 	private:
-		std::map<, unint16_t> m = // sample for getting the motherboard information
+		std::map<uint16_t, unint16_t> m = // sample for getting the motherboard information
 		{
-			{, EX_MOTHER_PORT_OUTPUT_SET_TYPE}
+			{X, EX_MOTHER_PORT_OUTPUT_SET_TYPE} //DEFINITION FOR X BUTTON
 		};
 	public:
 
--- src/main/include/button_definitions.h
+++ src/main/include/button_definitions.h
@@ -7,27 +7,27 @@
 
 //BUTTONS TODO: FIND INDEX IN THE JOY ARRAY
 
-#define X 
-#define Y 
-#define A 
-#define B 
-#define START 
-#define BACK 
-#define MODE
-#define LB
-#define RB 
-#define RIGHT_JOY_PRESS
-#define LEFT_JOY_PRESS
+#define X 2
+#define Y 3
+#define A 0
+#define B 1
+#define START 7 
+#define BACK 6
+#define MODE //DEFECT
+#define LB 4
+#define RB 5
+#define RIGHT_JOY_PRESS 10
+#define LEFT_JOY_PRESS 9
 
 //AXIS TODO: FIND INDEX IN THE JOY ARRAY
 
-#define LEFT_JOY_VERTICAL
-#define LEFT_JOY_HORIZONTAL
-#define RIGHT_JOY_VERTICAL
-#define RIGHT_JOY_HORIZONTAL
-#define ARROW_VERTICAL
-#define ARROW_HORIZONTAL
-#define LT
-#define RT
+#define LEFT_JOY_VERTICAL 1
+#define LEFT_JOY_HORIZONTAL 0
+#define RIGHT_JOY_VERTICAL 4
+#define RIGHT_JOY_HORIZONTAL 3
+#define ARROW_VERTICAL 7
+#define ARROW_HORIZONTAL 6
+#define LT 2
+#define RT 5
 
 #endif // end region of the include guard
